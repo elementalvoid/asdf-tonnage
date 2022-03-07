137f990a2553ebfd9fbe8c8e21ad6fec6eb5be6d
Matt Klich <matt@elementalvoid.com>
2022-03-06 17:06:05 -0700

    remove debug

--- lib/utils.bash
+++ lib/utils.bash
@@ -72,10 +72,6 @@ install_version() {
     mkdir -p "$install_path/bin"
     cp -r "$ASDF_DOWNLOAD_PATH"/${TOOL_NAME} "$install_path/bin/"
 
-    echo DEBUG
-    echo $install_path
-    ls $install_path
-
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
